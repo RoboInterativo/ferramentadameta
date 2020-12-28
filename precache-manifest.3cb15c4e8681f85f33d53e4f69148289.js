9fab9d8dd939eec08ca9009f75ec277cd3c109cc
Aleksey Shilo <chief_exb@mail.ru>
2020-12-29 02:09:32 +0300

    1

---
+++ precache-manifest.3cb15c4e8681f85f33d53e4f69148289.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05d4837db43a670c64b8dff6c081ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2342514eead73a9eda90",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "b2e92cfca670af41124b",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2342514eead73a9eda90",
+    "url": "/static/js/2.e176dca3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e176dca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e92cfca670af41124b",
+    "url": "/static/js/main.2dc1d464.chunk.js"
+  },
+  {
+    "revision": "c99a22dff43f698573f0",
+    "url": "/static/js/runtime-main.ae44c76a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
